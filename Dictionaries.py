47b6497309fd5f96f4aec2aa4b71bee5ecf5a3fd
Arpita Raj <singh.arpitaraj2001@gmail.com>
2026-05-05 22:49:43 +0530

    dictionary

---
+++ Dictionaries.py
@@ -0,0 +1,23 @@
+thisDict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisDict)
+
+# Dictionaries items
+print(thisDict["brand"])
+
+# Dictionaries 
+print(len(thisDict))
+
+# dictionaries type
+print(type(thisDict))
+
+# Accessing items
+X = thisDict["model"]
+print(X);
+
+# Get the value of the "model" key:
+
+X = thisDict.get("model");
